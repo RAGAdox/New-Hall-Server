58bf8993bd24b62af3361f78c50c097f81710d22
RAGAdox <rishitech882@gmail.com>
2019-04-09 00:33:59 +0530

    LOGIN MADE HARD CODED AUTH

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express=require('express');
 const exphbs = require("express-handlebars");
 const fileUpload=require('express-fileupload')
+const session=require('express-session')
+var cookieParser = require('cookie-parser');
 const path = require("path");
 var bodyParser = require("body-parser");
 const testFolder = "./uploads/";
 const router=require('./router/router.js');
 const app = express();
+app.use(cookieParser());
+app.use(session({
+    key: 'user_sid',
+    secret: 'somerandonstuffs',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        expires: 600000
+    }
+}));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(fileUpload());
 app.use('/',router);
--- router/router.js
+++ router/router.js
@@ -1,17 +1,26 @@
 const express=require('express');
 const fs = require("fs");
-
 const testFolder = "./uploads/";
-
+const user=require('../models/user')
 let filelist = [];
-const router = express();
+const router = express.Router();
 fs.readdir(testFolder, (err, files) => {
     files.forEach(file => {
       //console.log(file);
       filelist.push(file);
     });
   });
-router.get('/check',(req,res)=>{
+  var sessionChecker = (req, res, next) => {
+    console.log(req.session.user+' session checker')
+    console.log(req.cookies.user_sid+' cookies checker')
+    if (req.session.user || req.cookies.user_sid) {
+      next(); 
+      //res.redirect('/dashboard');
+    } else {
+       res.json({status:'not loged in'})
+    }    
+};
+router.get('/check',sessionChecker,(req,res)=>{
     res.json({status:"its ok"})
 })
 router.get('/',(req,res)=>{
@@ -20,7 +29,28 @@ router.get('/',(req,res)=>{
         filelist,
       })
 })
-
+router.get('/login',(req,res)=>{
+  res.render('login')
+})
+router.post('/login',(req,res)=>{
+  console.log(user)
+  if(req.body.username=='admin'&&req.body.password=='admin')
+  {
+    console.log('correct')
+    user.username=req.body.username
+    user.password=req.body.password
+    req.session.user = user
+    res.redirect('/')
+  }
+})
+router.get('/logout', (req, res) => {
+  if (req.session.user && req.cookies.user_sid) {
+      res.clearCookie('user_sid');
+      res.redirect('/');
+  } else {
+      res.redirect('/login');
+  }
+});
 router.get("/downloadFile/:file", (req, res) => {
     console.log(req.params.file);
     res.download("./uploads/" + req.params.file.substring(1), err => {
